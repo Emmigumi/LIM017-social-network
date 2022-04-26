645f9bac1485922381af422fef2fd5be26a349cd
Emmigumi <20160133@lamolina.edu.pe>
2022-04-26 18:18:48 -0500

    Add code main, add onnavigate.spec, register.spec- inconcluse

--- src/main.js
+++ src/main.js
@@ -13,32 +13,9 @@ const routes = {
   '/ForgotPasword': ResetPassword,
   '/Feed': Feed,
 };
-const rootDiv = document.getElementById('root');
-
-/* export const onNavigate = (pathname) => {
-  window.history.pushState(
-    {},
-    pathname,
-    window.location.origin + pathname,
-  );
-  while (rootDiv.firstChild) {
-    rootDiv.removeChild(rootDiv.firstChild);
-  }
-  return rootDiv.appendChild(routes[pathname]());
-};
-
-const component = routes[window.location.pathname];
-
-window.onpopstate = () => {
-  while (rootDiv.firstChild) {
-    rootDiv.removeChild(rootDiv.firstChild);
-  }
-  rootDiv.appendChild(routes[window.location.pathname]());
-};
-
-  rootDiv.appendChild(component()); */
 
 export const onNavigate = (pathname) => {
+  const rootDiv = document.getElementById('root');
   window.history.pushState({}, pathname, window.location.origin + pathname);
 
   while (rootDiv.firstChild) {
@@ -49,10 +26,8 @@ export const onNavigate = (pathname) => {
 };
 
 window.addEventListener('DOMContentLoaded', () => {
+  const rootDiv = document.getElementById('root');
   const component = routes[window.location.pathname];
-  while (rootDiv.firstChild) {
-    rootDiv.removeChild(rootDiv.firstChild);
-  }
   window.onpopstate = () => {
     rootDiv.appendChild(component());
   };
